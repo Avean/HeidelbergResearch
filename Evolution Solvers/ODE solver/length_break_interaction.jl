d7197553da6b9c6fd6b74ee882beea9f1bcdcdf6
hackelberry <finn@muennich.eu>
2025-11-11 09:24:34 +0100

    playing around trying to find prey predator model for length breaking interaction

--- Evolution Solvers/ODE solver/length_break_interaction.jl
+++ Evolution Solvers/ODE solver/length_break_interaction.jl
@@ -8,11 +8,11 @@ using Plots
 
 # --- Parameters ---
 const L = 1.0
-const l_crit = 5.0 # Need l_crit^(-1) - C_out < 0
+const l_crit = 5.0 # Need l_crit^(-1) - C_out < 0 (not anymore as we do not use l_des)
 const b_crit = 1.0
 const c_influx = 0.05
-const M_start = 0.7
-const C_out = 0.5
+const M_start = 0.5
+const C_out = 1.0
 
 # --- Define l_des(t) implicitly ---
 function compute_l_des(t)
@@ -25,25 +25,43 @@ function compute_l_des(t)
 end
 
 # --- ODE System ---
+# function nonlinear_system!(du, u, p, t)
+#     b, l, M = u
+#     # ldes = compute_l_des(t)
+#     du[1] = (- b + M*b)*(b_crit-b) #(ldes - l)
+#     term1 = (l - L) / (l * l_crit)
+#     term2 = M / l^2  # M = ((b_crit - b) * c_influx * t + M_start)
+#     du[2] = (-term1 + term2 - C_out) * (l_crit - l) #-term1 or +term1?
+#     du[3] = c_influx*(b_crit-b)*l - M*b^3 #produces oscillations: c_influx*M - M*b^3
+#     # Need *l in first term of du[3] in order to obtain different speed of oscillations
+#     # when C_out changed. Compare Fig.2 E in Ferenc 2021.
+# end
+
 function nonlinear_system!(du, u, p, t)
-    b, l, M = u
-    ldes = compute_l_des(t)
-    du[1] = (- b + M*b)*(b_crit-b) #(ldes - l)
-    term1 = (l - L) / (l * l_crit)
-    term2 = M / l^2  # M = ((b_crit - b) * c_influx * t + M_start)
-    du[2] = (term1 + term2 - C_out) * (l_crit - l)
-    du[3] = c_influx*(b_crit-b) - M*b^3
+    b, l, M, conc = u
+    # ldes = compute_l_des(t)
+    conc_in = M /l^2
+    du[1] = (- b + conc_in*b)*(b_crit-b) #(ldes - l)
+    press = (l - L) / (l * l_crit)
+    du[2] = (-press + conc_in - C_out) * (l_crit - l)
+    # du[3] = (c_influx*conc_in - 2*conc_in^2*du[2]*l - 2*conc_in*b^3*l^3) # equation for conc_in
+    du[3] = c_influx - b^3*conc_in  # equation for M
+    du[4] = (du[3]*conc - 2*conc^2*du[2]*l) / M # equation for conc insides, we have conc = conc_in = M/l^2.
 end
 
 # --- Initial Conditions and Time Span ---
-u0 = [0.1, 1.2, M_start]  # initial values for b and l
-tspan = (0.0, 100.0)  # from t = 0 to t = 10
+u0 = [0.01, L, M_start, M_start/L.^2]  # initial values for b and l and M (and conc_in: M_start/l^2)
+tspan = (0.0, 200.0)  # from t = 0 to t = 10
 
 # --- Define and solve the problem ---
 prob = ODEProblem(nonlinear_system!, u0, tspan)
-alg = Tsit5()
-sol = solve(prob, alg)
+# alg = Tsit5()
+sol = solve(prob)
 
 # --- Plot ---
-plot(sol, xlabel="Time", ylabel="Variables", label=["b(t)" "l(t)" "M(t)"], lw=2)
+plot(sol, xlabel="Time", ylabel="Variables", label=["b(t)" "l(t)" "M(t)" "conc(t)"], lw=2)
+l = sol[2,:]
+M = sol[3,:]
+concentration = M./l.^2
+plot!(sol.t, concentration, label="cont_in(t)")
 # ylims!(0,2)
