e5083e3a8f4a45b9a404de15aa0cceb6ade62f06
hackelberry <finn@muennich.eu>
2025-11-13 13:30:32 +0100

    second try bif diagram DKK model

--- Bifurcation Analysis/RD-ODE Hydra/bifurcation_diagram_Hydra.jl
+++ Bifurcation Analysis/RD-ODE Hydra/bifurcation_diagram_Hydra.jl
@@ -8,7 +8,7 @@ const BK = BifurcationKit
 # Laplacian operator in 1D with Neumann boundary conditions
 
 function Laplacian1D(Nx, lx)
-	hx = 2 * lx / Nx
+	hx = 2*lx / (Nx-1) # hx = 2 * lx / Nx
 	D2x = spdiagm(0 => -2 * ones(Nx), 1 => ones(Nx-1), -1 => ones(Nx-1)) / hx^2
 	D2x[1, 1] = -1 / hx^2
 	D2x[end, end] = -1 / hx^2
@@ -18,7 +18,7 @@ function Laplacian1D(Nx, lx)
 end
 
 # Define the nonlinearites
-nu = [0.0 3.8154e-05 0.4433 6.0713e-08 0.0004];
+nu = [0.0, 3.8154e-05, 0.4433, 6.0713e-08, 0.0004];
 beta = [1.0629 540.4003 1.1596 11.5964 11.5964 4.8254];
 F_nonl(Wl, A, Wd, C, S) = [beta[6]*S ./ ((1.0 .+ A).*(1.0 .+ C).*(1.0 .+ beta[3]*Wl)) - Wl,
                            beta[1] ./ (1.0 .+ beta[4]*Wl) - A,
@@ -28,7 +28,7 @@ F_nonl(Wl, A, Wd, C, S) = [beta[6]*S ./ ((1.0 .+ A).*(1.0 .+ C).*(1.0 .+ beta[3]
                           ]
 
 # Non-trivial constant steady state:
-U0 = [0.08167547924306925, 0.54587711524379, 3.6049476660047937, 1.8514050545898464, 0.08167547924306925]
+U0 = [0.08167547924306925, 0.54587711524379, 3.6049476660047937, 1.8514050545898464, 0.08167547924306925];
 
 function Fmit!(F, U, p)
 	N = p.N
@@ -48,7 +48,7 @@ function Fmit!(F, U, p)
     
     f3 = similar(Wd)
 	mul!(f3, p.Δ, Wd)
-	F[2N+1:3N] .= p.diffcoef*f3 .+ F_nonl(Wl, A, Wd, C, S)[3]
+	F[2N+1:3N] .= nu[3]*f3 .+ F_nonl(Wl, A, Wd, C, S)[3]
 
     f4 = similar(C)
 	mul!(f4, p.Δ, C)
@@ -56,33 +56,92 @@ function Fmit!(F, U, p)
 
     f5 = similar(S)
 	mul!(f5, p.Δ, S)
-	F[4N+1:5N] .= nu[5]*f5 .+ F_nonl(Wl, A, Wd, C, S)[5]
+	F[4N+1:5N] .= p.diffcoef*f5 .+ F_nonl(Wl, A, Wd, C, S)[5]
 
 	return F
 end
 
-# Jacobian of system:
-# delta(n) = n == 0
-# dϕ(u, κ, i, k) = (-1 + κ*exp(u[i]) / mean(exp.(u))) * delta(i-k) - κ*exp(u[i]) .* exp(u[k]) / (mean(exp.(u))*sum(exp.(u)))
+# assume:
+#   nu  :: Vector{Float64} length 5
+#   beta:: Vector{Float64} length 6
+#   p.Δ :: SparseMatrixCSC  (Nx×Nx Laplacian)
+#   p.N :: Int
+#   p.diffcoef :: Float64   (diffusion coefficient of S)
 
-# function JFmit(x,p)
-# 	J = p.Δ
-# 	dNonlin = zeros(length(x), length(x))
-# 	for i in 1:length(x)
-# 		for k in 1:length(x)
-# 			dNonlin[i, k] = dϕ(x, p.κ, i, k)
-# 		end
-# 	end
-# 	dNonlin = sparse(dNonlin)
-# 	return p.diffcoef*J + dNonlin
-# end
+function jacobian(U, p)
+    N = p.N
+    Δ = p.Δ
 
+    # Split variables
+    Wl = @view U[1:N]
+    A  = @view U[N+1:2N]
+    Wd = @view U[2N+1:3N]
+    C  = @view U[3N+1:4N]
+    S  = @view U[4N+1:5N]
+
+    # Preallocate blocks (sparse diagonals)
+    blocks = [spzeros(N,N) for _ in 1:25]
+    # helper to index block (row,col)
+    blk(r,c) = (r-1)*5 + c
+
+    # Fill diagonal entries of each block
+    @inbounds for i in 1:N
+        # pointwise nonlinear Jacobian
+        # Jnl = [
+        #     -beta[3]*beta[6]*S[i] / ((1+A[i])*(1+C[i])*(1+beta[3]*Wl[i])^2) - 1.0    -beta[6]*S[i] / ((1+A[i])^2*(1+C[i])*(1+beta[3]*Wl[i]))      0.0                 - beta[6]*S[i] / ((1+A[i])*(1+C[i])^2*(1+beta[3]*Wl[i]))     beta[6] / ((1+A[i])*(1+C[i])*(1+beta[3]*Wl[i]));
+        #     -beta[1]*beta[4] / (1+beta[4]*Wl[i])^2                                   -1.0                                                         0.0                   0.0                                                        0.0;
+        #      beta[2]*S[i]                                                             0.0                                                        -1.0                   0.0                                                        beta[2]*Wl[i];
+        #     -beta[5]*Wd[i] / (1+beta[5]*Wl[i])^2                                      0.0                                                         1/(1+beta[5]*Wl[i])  -1.0                                                        0.0;
+        #      1.0                                                                      0.0                                                         0.0                   0.0                                                       -1.0
+        # ]
+		Jnl = vcat(
+			hcat(
+				-beta[3]*beta[6]*S[i] / ((1+A[i])*(1+C[i])*(1+beta[3]*Wl[i])^2) - 1.0,
+				-beta[6]*S[i] / ((1+A[i])^2*(1+C[i])*(1+beta[3]*Wl[i])),
+				0.0,
+				-beta[6]*S[i] / ((1+A[i])*(1+C[i])^2*(1+beta[3]*Wl[i])),
+				beta[6] / ((1+A[i])*(1+C[i])*(1+beta[3]*Wl[i]))),
+			hcat(
+				-beta[1]*beta[4] / (1+beta[4]*Wl[i])^2,
+				-1.0, 0.0, 0.0, 0.0),
+			hcat(
+				beta[2]*S[i], 0.0, -1.0, 0.0, beta[2]*Wl[i]),
+			hcat(
+				-beta[5]*Wd[i] / (1+beta[5]*Wl[i])^2,
+				0.0, 1/(1+beta[5]*Wl[i]), -1.0, 0.0),
+			hcat(
+				1.0, 0.0, 0.0, 0.0, -1.0)
+		)
+
+        for r in 1:5, c in 1:5
+            blocks[blk(r,c)][i,i] = Jnl[r,c]
+        end
+    end
+
+    # Add diffusion (Laplace) terms on diagonals
+    blocks[blk(1,1)] += nu[1] * Δ
+    blocks[blk(2,2)] += nu[2] * Δ
+    blocks[blk(3,3)] += nu[3] * Δ
+    blocks[blk(4,4)] += nu[4] * Δ
+    blocks[blk(5,5)] += p.diffcoef * Δ
+
+    # Assemble 5×5 block matrix
+    Jnew = spzeros(5N,5N)
+    for r in 1:5, c in 1:5
+        rows = (r-1)*N+1 : r*N
+        cols = (c-1)*N+1 : c*N
+        Jnew[rows,cols] = blocks[blk(r,c)]
+    end
+
+    # copy!(J, Jnew)
+    return Jnew
+end
 
 ############################################################################################################################
 # Bifurcation Problem
 # Parameters:
-DiffCoef = 0.009;
-Nx = 50; lx = 0.5;
+DiffCoef = 0.0035;
+Nx = 10; lx = 0.5;
 Δ = Laplacian1D(Nx, lx);
 par_mit = (diffcoef = DiffCoef, Δ = Δ, N = Nx);
 sol0 = vcat(U0[1] * ones(Nx), U0[2] * ones(Nx), U0[3] * ones(Nx), U0[4] * ones(Nx), U0[5] * ones(Nx))
@@ -91,39 +150,54 @@ sol0 = vcat(U0[1] * ones(Nx), U0[2] * ones(Nx), U0[3] * ones(Nx), U0[4] * ones(N
 # Choose the weighted norm in order to break symmetries and see all branches.
 weight_norm = (lx .+ LinRange(-lx, lx, Nx)) |> vec
 w_one = ones(Nx) |> vec
-norm2(x) = norm(x .* w_one) / sqrt(length(x))
-norm2_weighted(x) = norm(x .* weight_norm) / sqrt(length(x))
+norm2(x) = norm(x .* w_one) / sqrt(length(x));
+norm2_weighted(x) = norm(x .* weight_norm) / sqrt(length(x));
 
-nev_N = 5*Nx
-int_param = [0.001, 0.5]
+# unweighted L2 over all species
+normC(x) = norm(x) / sqrt(length(x))
+# # weighted L2 emphasizing Wl
+# weight = ones(5*Nx)
+# weight[1:Nx] .= 1.0  # Wl
+# weight[Nx+1:5Nx] .= 0.5  # other species
+# normC(x) = norm(x .* weight) / sqrt(length(x))
+
 
 prob = BifurcationProblem(Fmit!, sol0, par_mit, (@optic _.diffcoef),;
-  record_from_solution = (x, p; k...) -> (nrm = norm2(x[1:Int(end/5)]), nw = norm2_weighted(x[1:Int(end/5)]), n∞ = norminf(x[1:Int(end/5)]), sol=x),
-  plot_solution = (x, p; k...) -> plot!(x[1:Int(end/5)] ; k...))
+	J = jacobian,
+	record_from_solution = (x, p; k...) -> (nrm = norm2(x[1:Int(end/5)]), nw = norm2_weighted(x[1:Int(end/5)]), n∞ = norminf(x[1:Int(end/5)]), sol=x),
+	plot_solution = (x, p; k...) -> plot!(x[1:Int(end/5)] ; k...))
 
+int_param = [0.003, 0.03]  # interval of continuation for the diffusion coefficient
+
+# Beispielwerte, anpassbar
+nev_N = 6; # number of eigenvalues to compute
+# eig_ncv = min(5*nev_N, 5*Nx*5);   # aber ≤ total dim; choose sensible cap
+# eig_ncv = min(eig_ncv, 5*Nx);     # ensure <= Ntot
+eig_ncv = 40;
+eig_tol = 1e-6;
+eig_maxiter = 2000;
 
 # eigensolver
-eigls = EigArpack()
-
+eigls = EigArpack(); #ncv = eig_ncv, tol = eig_tol, maxiter = eig_maxiter
 # options for Newton solver, we pass the eigen solver
-opt_newton = BK.NewtonPar(tol = 1e-8, verbose = true, eigsolver = eigls, max_iterations = 20)
+opt_newton = BK.NewtonPar(tol = 1e-8, verbose = true, eigsolver = eigls, max_iterations = 20);
 
 # options for continuation
 opts_br = ContinuationPar(p_min = int_param[1], p_max = int_param[2],
 	# for a good looking curve
-	dsmin = 0.0001, dsmax = 0.05, ds = 0.005,
+	dsmin = 1e-4, dsmax = 1e-2, ds = 1e-3,
 	# detect codim 1 bifurcations
-	detect_bifurcation = 3,
+	detect_bifurcation = 3,  # reduziert Kosten
     # number of eigenvalues to compute
 	nev = nev_N,
-    plot_every_step = 10,
+    plot_every_step = 20, 
 	newton_options = (@set opt_newton.verbose = false),
-    max_steps = 251,
+    max_steps = 200,
     tol_stability = 1e-6,
 	n_inversion = 6,
     # Optional: bisection options for locating bifurcations
-    dsmin_bisection = 1e-7, max_bisection_steps = 25, tol_bisection_eigenvalue = 1e-19
-    )
+    dsmin_bisection = 1e-7, max_bisection_steps = 25, tol_bisection_eigenvalue = 1e-8
+    );
 
 ############################################################################################################################
 # Calculating branches one by one.
@@ -139,14 +213,14 @@ for (index, point) in pairs(br.specialpoint)
 		continue
 	end
 	branches = continuation(br, index,
-		setproperties(opts_br; ds = 0.01), bothside=true ;
+		setproperties(opts_br), bothside=true ;
 		alg = PALC(),
 		nev = nev_N,
 	)
 	if branches == Branch[]
 		@show "No branches found for index $(index), try smaller ds"
 		branches = continuation(br, index,
-		setproperties(opts_br; ds = 0.001),
+		setproperties(opts_br; ds = 0.00001),
 		bothside=true ;
 		alg = PALC(),
 		nev = nev_N,
@@ -168,6 +242,12 @@ for branch in all_branches
 end
 plot(p1, p2)
 
+p2 = plot(br; vars = (:param, :n∞), title = "supremum norm")
+for branch in all_branches
+	p2 = plot!(branch; vars = (:param, :n∞))
+end
+plot(p1, p2)
+
 ######################################################################################################################
 # One can also use bifurcationdiagram() to compute the bifurcation diagram automatically and then by hand the missing branches.
 # automatic bifurcation diagram computation
@@ -175,13 +255,13 @@ diagram = @time bifurcationdiagram(prob, PALC(),
 	# very important parameter. This specifies the maximum amount of recursion
 	# when computing the bifurcation diagram. It means we allow computing branches of branches
 	# at most in the present case.
-	2,
+	3,
 	opts_br, bothside=true;
     verbosity = 0, plot = true,
     # callback_newton = cb,
-	usedeflation = true,
+	usedeflation = false,
 	# finalise_solution = finSol,
-	normC = norminf
+	normC = normC
 	)
 p4 = plot(diagram; plotfold = false, putspecialptlegend=false, markersize = 2, title = "#branches = $(size(diagram))", label="")
 # ylims!(p4, 0, 5)  # Specify the y-axis limits for the plot
